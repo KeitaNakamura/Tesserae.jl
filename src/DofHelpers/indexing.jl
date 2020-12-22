0842e3ef44cb7cd09145932337ec598a32c40464
Keita Nakamura <keita.nakamura.1109@gmail.com>
2020-12-22 23:23:49 +0900

    Modify PointToGridIndex to store dof indices

--- src/DofHelpers/indexing.jl
+++ src/DofHelpers/indexing.jl
@@ -8,20 +8,22 @@ struct PointToGridIndex{spacedim, G <: AbstractGrid{spacedim}}
     grid::G
     dofmap::DofMap{spacedim}
     dofindices::Vector{Vector{Int}}
+    dofindices_dim::Vector{Vector{Int}}
     gridindices::Vector{Vector{CartesianIndex{spacedim}}}
 end
 
 function PointToGridIndex(npoints::Int, grid::AbstractGrid{spacedim}) where {spacedim}
     dofmap = DofMap(size(grid))
     dofindices = [Int[] for _ in 1:npoints]
+    dofindices_dim = [Int[] for _ in 1:npoints]
     gridindices = [CartesianIndex{spacedim}[] for _ in 1:npoints]
-    PointToGridIndex(grid, dofmap, dofindices, gridindices)
+    PointToGridIndex(grid, dofmap, dofindices, dofindices_dim, gridindices)
 end
 
 """
-    numbering!(::PointToGridIndex, coordinates::AbstractArray{<: Vec}; exclude = nothing, dim = spacedim)
+    numbering!(::PointToGridIndex, coordinates::AbstractArray{<: Vec}; point_radius::Real, exclude = nothing)
 
-Numbering point-to-dof and grid indices by point `coordinates`.
+Numbering point-to-dof and point-to-grid indices by `coordinates` of points.
 `point_radius` is `h` in [`neighboring_nodes(grid, x::Vec, h::Real)`](@ref).
 
 # Examples
@@ -33,15 +35,11 @@ julia> pg = PointToGridIndex(length(points), Grid(0:4, 0:3));
 julia> numbering!(pg, points; point_radius = 1);
 
 julia> dofindices(pg, 1)
-8-element Array{Int64,1}:
+4-element Array{Int64,1}:
  1
  2
  3
  4
- 5
- 6
- 7
- 8
 
 julia> dofindices(pg, 2)
 Int64[]
@@ -57,10 +55,11 @@ julia> gridindices(pg, 2)
 CartesianIndex{2}[]
 ```
 """
-function numbering!(pg::PointToGridIndex{spacedim}, coordinates::AbstractArray{<: Vec{spacedim}}; exclude = nothing, point_radius::Real, dim::Int = spacedim) where {spacedim}
+function numbering!(pg::PointToGridIndex{dim}, coordinates::AbstractArray{<: Vec{dim}}; exclude = nothing, point_radius::Real) where {dim}
     dofindices = pg.dofindices
+    dofindices_dim = pg.dofindices_dim
     gridindices = pg.gridindices
-    @assert length(coordinates) == length(dofindices) == length(gridindices)
+    @assert length(coordinates) == length(dofindices) == length(dofindices_dim) == length(gridindices)
 
     grid = pg.grid
     dofmap = pg.dofmap
@@ -96,7 +95,8 @@ function numbering!(pg::PointToGridIndex{spacedim}, coordinates::AbstractArray{<
     # Reinitialize interpolations by updated mask
     @inbounds for (i, x) in enumerate(coordinates)
         allinds = neighboring_nodes(grid, x, point_radius)
-        map!(dofmap, dofindices[i], allinds; dim)
+        map!(dofmap, dofindices[i], allinds)
+        map!(dofmap, dofindices_dim[i], allinds; dim)
         filter!(dofmap, gridindices[i], allinds)
     end
 
@@ -105,15 +105,25 @@ end
 
 """
     dofindices(::PointToGridIndex, p::Int)
+    dofindices(::PointToGridIndex, p::Int, dim::Int)
 
-Return stored dof indices at point index `p`.
+Return dof indices at point index `p`.
+Use [`numbering!(::PointToGridIndex, ::AbstractArray{<: Vec})`](@ref) in advance.
 """
 dofindices(pg::PointToGridIndex, p::Int) = (@_propagate_inbounds_meta; pg.dofindices[p])
+
+function dofindices(pg::PointToGridIndex{spacedim}, p::Int, dim::Int) where {spacedim}
+    @_propagate_inbounds_meta
+    spacedim == 1   && return pg.dofindices[p]
+    spacedim == dim && return pg.dofindices_dim[p]
+    map(pg.dofmap, pg.gridindices[p])
+end
 
 """
     gridindices(::PointToGridIndex, p::Int)
 
-Return stored grid indices at point index `p`.
+Return grid indices at point index `p`.
+Use [`numbering!(::PointToGridIndex, ::AbstractArray{<: Vec})`](@ref) in advance.
 """
 gridindices(pg::PointToGridIndex, p::Int) = (@_propagate_inbounds_meta; pg.gridindices[p])
 
