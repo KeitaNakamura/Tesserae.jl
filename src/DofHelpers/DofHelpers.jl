09bd1dfce7ee1973e348cf7cc2951f1c94802252
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-01-02 19:05:23 +0900

    Remove PointToGridIndex

--- src/DofHelpers/DofHelpers.jl
+++ src/DofHelpers/DofHelpers.jl
@@ -1,21 +1,13 @@
 module DofHelpers
 
-using Jams.Grids
 using Base: @_propagate_inbounds_meta
 
 export
-# DofMap
     DofMap,
     DofMapIndices,
     ndofs,
-    indices,
-# PointToGridIndex
-    PointToGridIndex,
-    numbering!,
-    dofindices,
-    gridindices
+    indices
 
 include("dofmap.jl")
-include("indexing.jl")
 
 end
--- src/DofHelpers/dofmap.jl
+++ src/DofHelpers/dofmap.jl
@@ -241,3 +241,5 @@ Base.parent(I::DofMapIndices) = DofMap(I.indices)
 Base.IndexStyle(::Type{<: DofMapIndices}) = IndexLinear()
 Base.size(I::DofMapIndices) = size(I.indices)
 Base.getindex(I::DofMapIndices, i::Int) = (@_propagate_inbounds_meta; j = I.indices[i]; ifelse(j == -1, nothing, j))
+
+ndofs(I::DofMapIndices; dof = 1) = ndofs(parent(I); dof)
