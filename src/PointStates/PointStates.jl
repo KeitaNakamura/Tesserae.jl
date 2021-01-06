a45ed82fe4c7f5d3e664ecd22b79b90b4e05fe78
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-01-06 20:34:38 +0900

    Add generate_pointstates function

--- src/PointStates/PointStates.jl
+++ src/PointStates/PointStates.jl
@@ -1,14 +1,18 @@
 module PointStates
 
 using Jams.Arrays: AbstractCollection, UnionCollection, LazyCollection, lazy
+using Jams.Grids
 using Base: @_propagate_inbounds_meta
 using Base.Broadcast: broadcasted
 
 export
 # PointState
     PointState,
-    ←
+    ←,
+# generation
+    generate_pointstates
 
 include("pointstate.jl")
+include("generation.jl")
 
 end
---
+++ src/PointStates/generation.jl
@@ -0,0 +1,25 @@
+function generate_pointstates(indomain, grid::AbstractGrid{dim, T}, coordinate_system = :plane_strain_if_2D; n::Int = 2) where {dim, T}
+    h = gridsteps(grid) ./ n # length per particle
+    allpoints = Grid(StepRangeLen.(first.(gridaxes(grid)) .+ h./2, h, n .* (size(grid) .- 1) .- 1))
+    npoints = count(x -> indomain(x...), allpoints)
+    xₚ = PointState(Vec{dim, T}, npoints)
+    Vₚ = PointState(T, npoints)
+    hₚ = PointState(Vec{dim, T}, npoints)
+    i = 0
+    for x in allpoints
+        if indomain(x...)
+            xₚ[i+=1] = x
+        end
+    end
+    V = prod(h)
+    for i in 1:npoints
+        if dim == 2 && coordinate_system == :axisymmetric
+            r = xₚ[i][1]
+            Vₚ[i] = r * V
+        else
+            Vₚ[i] = V
+        end
+        hₚ[i] = Vec(h)
+    end
+    (; xₚ, Vₚ, hₚ)
+end
