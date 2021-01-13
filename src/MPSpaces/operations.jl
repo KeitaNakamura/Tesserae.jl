33357975ecc15dd23844669e2e8312f02ec8f053
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-01-13 09:39:56 +0900

    Remove PointToGridMatrixOperation

--- src/MPSpaces/operations.jl
+++ src/MPSpaces/operations.jl
@@ -1,58 +1,55 @@
 struct PointToGridOperation{C <: AbstractCollection{2}}
-    u_i::C
+    gridvalues::C
+    ismatrix::Bool
 end
 
-struct PointToGridMatrixOperation{C <: AbstractCollection{2}}
-    K_ij::C
-end
+gridvalues(c::PointToGridOperation, p::Int) = (@_propagate_inbounds_meta; c.gridvalues[p])
+ismatrix(c::PointToGridOperation) = c.ismatrix
 
 function ∑ₚ(c::AbstractCollection{2})
     ElType = eltype(c)
     if ElType <: AbstractCollection{0}
-        return PointToGridOperation(c)
+        return PointToGridOperation(c, false)
     elseif ElType <: AbstractCollection{-1}
-        return PointToGridMatrixOperation(c)
+        return PointToGridOperation(c, true)
     end
     throw(ArgumentError("wrong collection in ∑ₚ"))
 end
 
 for op in (:+, :-)
     @eval function Base.$op(x::PointToGridOperation, y::PointToGridOperation)
-        PointToGridOperation($op(x.u_i, y.u_i))
+        PointToGridOperation($op(x.gridvalues, y.gridvalues))
     end
 end
 
 function set!(S::GridState, x::PointToGridOperation)
+    @assert !ismatrix(x)
     nzval = nonzeros(zeros!(S))
     dofinds = S.dofindices
     @inbounds for p in eachindex(dofinds)
         u = view(nzval, dofinds[p])
-        u .+= x.u_i[p]
+        u .+= gridvalues(x, p)
     end
     S
 end
 
-function set!(S::GridStateMatrix{Vec{dim, T}}, x::PointToGridMatrixOperation) where {dim, T}
+_to_matrix(x::Real, dim::Int) = ScalarMatrix(x, dim, dim)
+_to_matrix(x::Tensor{2}, dim::Int) = x
+_get_element(mat, index, dim::Int) = (@_propagate_inbounds_meta; _to_matrix(mat.bc[index], dim))
+_compute_range(dofs::Vector{Int}, i::Int, dim::Int) =
+    (@_propagate_inbounds_meta; start = dim*(dofs[i]-1) + 1; start:start+dim-1)
+function set!(S::GridStateMatrix{Vec{dim, T}}, x::PointToGridOperation) where {dim, T}
+    @assert ismatrix(x)
     empty!(S)
     dofinds = S.dofindices
-    compute_range = function(dofs, i)
-        @_propagate_inbounds_meta
-        start = dim*(dofs[i]-1) + 1
-        start:start+dim-1
-    end
-    get_element = if eltype(first(x.K_ij)) <: Real
-        (mat, index) -> ScalarMatrix(mat.bc[index], dim, dim)
-    else
-        (mat, index) -> mat.bc[index]
-    end
     @inbounds for p in eachindex(dofinds)
-        mat = x.K_ij[p]
+        mat = gridvalues(x, p)
         dofs = dofinds[p]
         for index in CartesianIndices(size(mat))
             i, j = Tuple(index)
-            I = compute_range(dofs, i)
-            J = compute_range(dofs, j)
-            push!(S, get_element(mat, index), I, J)
+            I = _compute_range(dofs, i, dim)
+            J = _compute_range(dofs, j, dim)
+            push!(S, _get_element(mat, index, dim), I, J)
         end
     end
     S
@@ -60,7 +57,7 @@ end
 
 for op in (:*, :/)
     @eval function Base.$op(x::PointToGridOperation, y::GridState)
-        PointToGridOperation($op(x.u_i, GridCollection(y)))
+        PointToGridOperation($op(x.gridvalues, GridCollection(y)))
     end
 end
 
