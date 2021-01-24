9818801682c81c295ee949fa5a228461c44fc7b1
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-01-24 19:03:19 +0900

    Modify tests for assembling

--- src/States/GridToPointOperation.jl
+++ src/States/GridToPointOperation.jl
@@ -3,19 +3,12 @@ struct GridToPointOperation{C <: AbstractCollection{2}} <: AbstractCollection{2}
 end
 
 function ∑ᵢ(c::AbstractCollection{2})
-    # Using `reduce` causes type instability in some cases
+    # TODO: fix type instability
     GridToPointOperation(lazy(sum, c))
 end
 
 Base.length(x::GridToPointOperation) = length(x.u_p)
 Base.getindex(x::GridToPointOperation, i::Int) = (@_propagate_inbounds_meta; x.u_p[i])
-
-function set!(ps::PointState, x::GridToPointOperation)
-    @inbounds for p in 1:length(ps)
-        ps[p] = x.u_p[p]
-    end
-    ps
-end
 
 function Base.add_sum(a::ScalVec{dim, T}, b::ScalVec{dim, T})::ScalVec{dim, T} where {dim, T}
     ScalVec(a.x + b.x, a.∇x + b.∇x)
--- test/assembling.jl
+++ test/assembling.jl
@@ -3,6 +3,37 @@ module AssemblingTest
 using Jams
 using Test, LinearAlgebra
 
+function check_point_to_grid(x, N)
+    @test (@inferred x[1]) |> Array |> ndims == N
+end
+
+function check_grid_to_point(x, T)
+    # TODO: fix type instability
+    # (@inferred x[1])::T
+end
+
+@testset "Point-to-grid assembling" begin
+    grid = Grid(0:0.1:1.0, 0:0.1:1.0)
+    space = MPSpace(LinearBSpline(dim=2), grid, 10)
+    xₚ = pointstate(space, Vec{2, Float64})
+    for i in 1:length(xₚ)
+        xₚ[i] = rand(Vec{2})
+    end
+    N = construct(:shape_value, space)
+    reinit!(space, xₚ)
+
+    mₚ = pointstate(space, Float64)
+    check_point_to_grid((@inferred ∑ₚ(N)), 1)
+    check_point_to_grid((@inferred ∑ₚ(N*xₚ)), 1)
+    check_point_to_grid((@inferred ∑ₚ(N*N)), 2)
+    check_point_to_grid((@inferred ∑ₚ(N*mₚ*N)), 2)
+    check_point_to_grid((@inferred ∑ₚ(∇(N)⋅∇(N))), 2)
+    check_point_to_grid((@inferred ∑ₚ(∇(N)*mₚ⋅∇(N))), 2)
+
+    vᵢ = pointstate(space, Vec{2, Float64})
+    check_grid_to_point((@inferred ∑ᵢ(N*vᵢ)), Vec{2,Float64})
+end
+
 @testset "Assembling global matrix" begin
     grid = Grid(0:0.1:1.0, 0:0.1:1.0)
     space = MPSpace(LinearBSpline(dim=2), grid, 10)
@@ -16,7 +47,9 @@ using Test, LinearAlgebra
 
     # Test 1
     A = gridstate_matrix(space, Mat{2,2,Float64}); reinit!(A)
-    A ← ∑ₚ(∇(N) ⊗ ∇(N))
+    ex = ∑ₚ(∇(N) ⊗ ∇(N))
+    (@inferred ex[1])::AbstractCollection{-1}
+    A ← ex
     B = SparseMatrixCOO()
     for p in 1:npoints(space)
         ∇Nᵢ = vcat(∇(N)[p]...)
@@ -31,7 +64,9 @@ using Test, LinearAlgebra
         cₚ[i] = rand(SymmetricFourthOrderTensor{2})
     end
     A = gridstate_matrix(space, Mat{2,2,Float64}); reinit!(A)
-    A ← ∑ₚ(dotdot(∇(N), cₚ, ∇(N)))
+    ex = ∑ₚ(dotdot(∇(N), cₚ, ∇(N)))
+    (@inferred ex[1])::AbstractCollection{-1}
+    A ← ex
     B = SparseMatrixCOO()
     for p in 1:npoints(space)
         Nᵢ = N[p]
@@ -54,9 +89,13 @@ using Test, LinearAlgebra
     B = gridstate_matrix(space, Mat{2,2,Float64}); reinit!(B)
     mᵢ = gridstate(space, Float64)
     # consisten mass matrixt
-    A ← ∑ₚ(N*N)
+    ex = ∑ₚ(N*N)
+    (@inferred ex[1])::AbstractCollection{-1}
+    A ← ex
     # lumped mass matrix (using gridstate)
-    mᵢ ← ∑ₚ(N)
+    ex = ∑ₚ(N)
+    (@inferred ex[1])::AbstractCollection{0}
+    mᵢ ← ex
     B ← GridDiagonal(mᵢ)
     @test Diagonal(vec(sum(sparse(A), dims = 2))) ≈ sparse(B)
     # lumped mass matrix (without temporary array)
