c91002048ec675698551c09226359138c1cd2ac3
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-02-08 09:02:12 +0900

    Fix type instability in ∑ᵢ
    * Type instability can be fixed by manually calling getindex of Broadcated object
    * This fix, however, dosen't affect on the performance (why?)

--- src/States/GridToPointOperation.jl
+++ src/States/GridToPointOperation.jl
@@ -3,12 +3,17 @@ struct GridToPointOperation{C <: AbstractCollection{2}} <: AbstractCollection{2}
 end
 
 function ∑ᵢ(c::AbstractCollection{2})
-    # TODO: fix type instability
     GridToPointOperation(lazy(sum, c))
 end
 
 Base.length(x::GridToPointOperation) = length(x.u_p)
 Base.getindex(x::GridToPointOperation, i::Int) = (@_propagate_inbounds_meta; x.u_p[i])
+function Base.getindex(x::GridToPointOperation{<: LazyCollection{2}}, i::Int)
+    @_propagate_inbounds_meta
+    bc = x.u_p.bc
+    args = Base.Broadcast._getindex(bc.args, i)
+    Base.Broadcast._broadcast_getindex_evalf(bc.f, args...)
+end
 
 function Base.add_sum(a::ScalVec{dim, T}, b::ScalVec{dim, T})::ScalVec{dim, T} where {dim, T}
     ScalVec(a.x + b.x, a.∇x + b.∇x)
