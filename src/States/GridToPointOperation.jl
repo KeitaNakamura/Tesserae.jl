342d27a3bd3f38afc75bee04cf4bb4b0f6e311d4
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-02-08 09:05:41 +0900

    Use simd computation in ∑ᵢ

--- src/States/GridToPointOperation.jl
+++ src/States/GridToPointOperation.jl
@@ -3,7 +3,7 @@ struct GridToPointOperation{C <: AbstractCollection{2}} <: AbstractCollection{2}
 end
 
 function ∑ᵢ(c::AbstractCollection{2})
-    GridToPointOperation(lazy(sum, c))
+    GridToPointOperation(lazy(_sum, c))
 end
 
 Base.length(x::GridToPointOperation) = length(x.u_p)
@@ -15,9 +15,18 @@ function Base.getindex(x::GridToPointOperation{<: LazyCollection{2}}, i::Int)
     Base.Broadcast._broadcast_getindex_evalf(bc.f, args...)
 end
 
-function Base.add_sum(a::ScalVec{dim, T}, b::ScalVec{dim, T})::ScalVec{dim, T} where {dim, T}
+function _sum(c)
+    x = first(c)
+    @simd for i in 2:length(c)
+        @inbounds x = add(x, c[i])
+    end
+    x
+end
+
+add(a, b) = a + b
+function add(a::ScalVec{dim, T}, b::ScalVec{dim, T})::ScalVec{dim, T} where {dim, T}
     ScalVec(a.x + b.x, a.∇x + b.∇x)
 end
-function Base.add_sum(a::VecTensor{dim, T, M}, b::VecTensor{dim, T, M})::VecTensor{dim, T, M} where {dim, T, M}
+function add(a::VecTensor{dim, T, M}, b::VecTensor{dim, T, M})::VecTensor{dim, T, M} where {dim, T, M}
     VecTensor(a.x + b.x, a.∇x + b.∇x)
 end
