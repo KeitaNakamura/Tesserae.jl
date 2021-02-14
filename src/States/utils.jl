a32a83d77f5a52cb69232eb179a9dc21fb85c362
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-02-14 14:42:59 +0900

    Modify colon computations

--- src/States/utils.jl
+++ src/States/utils.jl
@@ -5,16 +5,13 @@ which_type(::Vararg{Type{<: UnionGridState}}) = UnionGridState
 which_type(::Vararg) = error()
 ## colon computation
 (::Colon)(op, x::Union{AbstractCollection{2}, UnionGridState}) = lazy(op, x)
-(::Colon)(op, xs::Tuple) = lazy(op, xs...)
-# @generated function (::Colon)(op, xs::Tuple)
-    # t = which_type([x for x in xs.parameters if x <: Union{AbstractCollection{2}, UnionGridState}]...)
-    # if t <: AbstractCollection{2}
-        # exps = [x <: AbstractCollection{2} ? :(xs[$i]) : :(Ref(xs[$i])) for (i,x) in enumerate(xs.parameters)]
-        # return :(lazy(broadcasted(op, $(exps...))))
-    # else t <: UnionGridState
-        # states = [:(xs[$i]) for (i,x) in enumerate(xs.parameters) if x <: UnionGridState]
-        # exps = [x <: UnionGridState ? :(_collection(nonzeros(xs[$i]))) : :(Ref(xs[$i])) for (i,x) in enumerate(xs.parameters)]
-        # return :(GridStateOperation(indices($(states...)), dofindices($(states...)), LazyCollection{1}(broadcasted(op, $(exps...)))))
-    # end
-# end
-
+@generated function (::Colon)(op, xs::Tuple)
+    t = which_type([x for x in xs.parameters if x <: Union{AbstractCollection{2}, UnionGridState}]...)
+    if t <: AbstractCollection{2}
+        return :(lazy(op, xs...))
+    else t <: UnionGridState
+        states = [:(xs[$i]) for (i,x) in enumerate(xs.parameters) if x <: UnionGridState]
+        exps = [x <: UnionGridState ? :(_collection(nonzeros(xs[$i]))) : :(Ref(xs[$i])) for (i,x) in enumerate(xs.parameters)]
+        return :(GridStateOperation(indices($(states...)), dofindices($(states...)), LazyCollection(op, $(exps...))))
+    end
+end
