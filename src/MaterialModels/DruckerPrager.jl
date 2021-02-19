fae70fe21c58898de5817b68d88844439b1425a8
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-02-19 13:46:26 +0900

    Allow to use SoilElastic for DruckerPrager

--- src/MaterialModels/DruckerPrager.jl
+++ src/MaterialModels/DruckerPrager.jl
@@ -1,16 +1,16 @@
-struct DruckerPrager{T} <: MaterialModel
-    elastic::LinearElastic{T}
+struct DruckerPrager{T, Elastic <: Union{LinearElastic{T}, SoilElastic{T}}} <: MaterialModel
+    elastic::Elastic
     A::T
     B::T
     b::T
 end
 
-function DruckerPrager(elastic::LinearElastic; A::Real, B::Real, b::Real = B)
+function DruckerPrager(elastic; A::Real, B::Real, b::Real = B)
     DruckerPrager(elastic, A, B, b)
 end
 
 # for Mohr-Coulomb criterion
-function DruckerPrager(elastic::LinearElastic, mc_type::Symbol; c::Real, ϕ::Real, ψ::Real = ϕ)
+function DruckerPrager(elastic, mc_type::Symbol; c::Real, ϕ::Real, ψ::Real = ϕ)
     ϕ = deg2rad(ϕ)
     ψ = deg2rad(ψ)
     if mc_type == :circumscribed
@@ -31,7 +31,7 @@ function DruckerPrager(elastic::LinearElastic, mc_type::Symbol; c::Real, ϕ::Rea
     DruckerPrager(elastic; A, B, b)
 end
 
-function update_stress(model::DruckerPrager, σ::SymmetricSecondOrderTensor{3}, dϵ::SymmetricSecondOrderTensor{3})::typeof(dϵ)
+function update_stress(model::DruckerPrager{<: Any, <: LinearElastic}, σ::SymmetricSecondOrderTensor{3}, dϵ::SymmetricSecondOrderTensor{3})::typeof(dϵ)
     # compute the stress at the elastic trial state
     De = model.elastic.D
     σ_trial = σ + De ⊡ dϵ
@@ -44,6 +44,72 @@ function update_stress(model::DruckerPrager, σ::SymmetricSecondOrderTensor{3}, 
     # compute the stress
     σ_trial - Δγ * (De ⊡ dgdσ)
 end
+
+function update_stress(model::DruckerPrager{<: Any, <: SoilElastic}, σ::SymmetricSecondOrderTensor{3, T}, dϵ::SymmetricSecondOrderTensor{3})::typeof(dϵ) where {T}
+    # compute the stress at the elastic trial state
+    σₙ = σ
+    ϵᵉ = compute_elastic_strain(model.elastic, σ)
+    ϵᵉ_trial = ϵᵉ + dϵ
+    σ_trial = compute_stress(model.elastic, ϵᵉ_trial)
+    yield_function(model, σ_trial) ≤ 0.0 && return σ_trial
+
+    # prepare solution vector x
+    σ = σ_trial
+    Δγ = zero(T)
+    for i in 1:20
+        dfdσ, f = gradient(σ -> yield_function(model, σ), σ, :all)
+        dNdσ, N = gradient(σ -> plastic_flow(model, σ), σ, :all)
+        # cᵉ, ϵᵉ = gradient(σ -> ∇W̃(model.elastic, σ), σ, :all)
+        cᵉ = ∇²W̃(model.elastic, σ)
+        ϵᵉ = ∇W̃(model.elastic, σ)
+
+        R = ϵᵉ - ϵᵉ_trial + Δγ*N
+        # @show norm(R)
+        norm(R) < sqrt(eps(T)) && abs(f) < sqrt(eps(T)) && break
+
+        Ξ = inv(cᵉ + Δγ*dNdσ)
+        dfdσ_Ξ = dfdσ ⊡ Ξ
+        dΔγ = (f - dfdσ_Ξ ⊡ R) / (dfdσ_Ξ ⊡ N)
+
+        Δγ += dΔγ
+        dσ = Ξ ⊡ (-R - dΔγ * N)
+        σ += dσ
+    end
+    σ
+end
+
+#=
+function residual_vector(model::DruckerPrager, x, ϵᵉ_trial)
+    @inbounds begin
+        σ = frommandel(SymmetricSecondOrderTensor{3}, view(x, 1:6))
+        Δγ = x[7]
+    end
+    ϵᵉ = compute_elastic_strain(model.elastic, σ)
+    f = yield_function(model, σ)
+    dgdσ = plastic_flow(model, σ)
+    Vec{7}(Tuple(tomandel(ϵᵉ_trial - ϵᵉ - Δγ*dgdσ))..., f)
+end
+
+function update_stress(model::DruckerPrager{<: Any, <: SoilElastic}, σ::SymmetricSecondOrderTensor{3, T}, dϵ::SymmetricSecondOrderTensor{3})::typeof(dϵ) where {T}
+    # compute the stress at the elastic trial state
+    ϵᵉ = compute_elastic_strain(model.elastic, σ)
+    ϵᵉ_trial = ϵᵉ + dϵ
+    σ_trial = compute_stress(model.elastic, ϵᵉ_trial)
+    yield_function(model, σ_trial) ≤ 0.0 && return σ_trial
+
+    # prepare solution vector x
+    x = Vec{7}(Tuple(tomandel(σ_trial))..., zero(T))
+
+    # plastic corrector
+    for i in 1:20 # should be converged in 20 iterations
+        J, r = gradient(x -> residual_vector(model, x, ϵᵉ_trial), x, :all)
+        norm(r) < sqrt(eps(T)) && break
+        # update states
+        x += -J \ r
+    end
+    @inbounds frommandel(SymmetricSecondOrderTensor{3}, view(x, 1:6))
+end
+=#
 
 function yield_function(model::DruckerPrager, σ::SymmetricSecondOrderTensor{3})::eltype(σ)
     A = model.A
