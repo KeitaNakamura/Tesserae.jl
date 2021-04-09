e195944f766a56814b889cb07677b8ddc40c89f0
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-04-09 20:14:26 +0900

    Change name of parameter K to B in WaterModel

--- src/MaterialModels/WaterModel.jl
+++ src/MaterialModels/WaterModel.jl
@@ -1,18 +1,18 @@
 struct WaterModel{T} <: MaterialModel
-    K::T
+    B::T
     γ::T
 end
 
 # https://www.researchgate.net/publication/220789258_Weakly_Compressible_SPH_for_Free_Surface_Flows
-# K = 1119 (kPa)
+# B = 1119 (kPa)
 # γ = 7
-function WaterModel(; K::Real = 1119e3, γ::Real = 7)
-    WaterModel(K, γ)
+function WaterModel(; B::Real = 1119e3, γ::Real = 7)
+    WaterModel(B, γ)
 end
 
 function update_stress(model::WaterModel, σ::SymmetricSecondOrderTensor{3}, F::SecondOrderTensor{3})
-    K = model.K
+    B = model.B
     γ = model.γ
-    p = K * (1/det(F)^γ - 1)
+    p = B * (1/det(F)^γ - 1)
     -p*one(σ)
 end
