85754b162ceab8f2e009eb3aac753d699799df35
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-06-18 13:07:06 +0900

    Fix gridstep -> gridsteps

--- src/ShapeFunctions/WLSValue.jl
+++ src/ShapeFunctions/WLSValue.jl
@@ -30,7 +30,7 @@ function reinit!(it::WLSValue{<: Any, <: Any, dim}, grid::AbstractGrid{dim}, ind
     resize!(it.w, length(indices))
     @inbounds for (j, I) in enumerate(view(CartesianIndices(grid), indices))
         xᵢ = grid[I]
-        ξ = (x - xᵢ) ./ gridstep(grid)
+        ξ = (x - xᵢ) ./ gridsteps(grid)
         it.w[j] = F(ξ)
     end
     P = polynomial(it)
