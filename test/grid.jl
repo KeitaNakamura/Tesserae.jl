9cebc9f418167a8894ba9a40afa50a3076e10635
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-08-16 14:55:11 +0900

    Add tests for neighboring_nodes, neighboring_cells and whichcell

--- test/grid.jl
+++ test/grid.jl
@@ -14,4 +14,11 @@
     @test @inferred(gridaxes(grid, 1)) === 0:1.0:10
     @test @inferred(gridaxes(grid, 2)) === 1:2.0:20
     @test @inferred(gridorigin(grid))::Vec{2} == Vec(0,1)
+
+    @test @inferred(Poingr.neighboring_nodes(grid, Vec(0.6, 8.8), 1))::CartesianIndices == CartesianIndices((1:2, 4:5))
+    @test @inferred(Poingr.neighboring_nodes(grid, Vec(0.6, 8.8), 2))::CartesianIndices == CartesianIndices((1:3, 3:6))
+    @test @inferred(Poingr.neighboring_cells(grid, Vec(0.6, 8.8), 1))::CartesianIndices == CartesianIndices((1:2, 3:5))
+    @test @inferred(Poingr.neighboring_cells(grid, Vec(0.6, 8.8), 2))::CartesianIndices == CartesianIndices((1:3, 2:6))
+    @test (Poingr.whichcell(grid, Vec(0.6, 8.8)))::CartesianIndex == CartesianIndex(1, 4)
+    @test (Poingr.whichcell(grid, Vec(-0.6, 8.8)))::Nothing == nothing
 end
