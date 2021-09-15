daf2f5f3b41c8634a40130f967f7135aa5e3e7ae
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-09-15 14:52:54 +0900

    Add tests for WLSValues (#21)

--- test/shapefunctions.jl
+++ test/shapefunctions.jl
@@ -13,3 +13,17 @@
         end
     end
 end
+
+@testset "WLSValues" begin
+    for dim in 1:2
+        grid = Grid(ntuple(i -> 0.0:0.1:5.0, Val(dim)))
+        for bspline in (QuadraticBSpline{dim}(), CubicBSpline{dim}(),)
+            it = Poingr.ShapeValues(WLS{1}(bspline))
+            for x in Iterators.product(ntuple(i -> 0.0:0.05:5.0, Val(dim))...)
+                reinit!(it, grid, Vec(x))
+                @test sum(it.N) ≈ 1
+                @test sum(it.∇N) ≈ zero(Vec{dim}) atol = sqrt(eps(Float64))
+            end
+        end
+    end
+end
