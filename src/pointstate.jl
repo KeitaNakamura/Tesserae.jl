86044702530c8393666fd54944a0f7cb0056f1d4
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-09-30 14:37:30 +0900

    Modify `generate_pointstate!` (#68)

--- src/pointstate.jl
+++ src/pointstate.jl
@@ -1,31 +1,40 @@
 function generate_pointstate(indomain, Point::Type, grid::Grid{dim, T}, coord_system::CoordinateSystem = DefaultCoordinateSystem(); n::Int = 2) where {dim, T}
     h = gridsteps(grid) ./ n # length per particle
-    allpoints = Grid(LinRange.(first.(gridaxes(grid)) .+ h./2, last.(gridaxes(grid)) .- h./2, n .* (size(grid) .- 1)))
+    allpoints = Grid(@. LinRange(
+        first($gridaxes(grid)) + h/2,
+        last($gridaxes(grid))  - h/2,
+        n * ($size(grid) - 1)
+    ))
 
     npoints = count(x -> indomain(x...), allpoints)
     pointstate = reinit!(StructVector{Point}(undef, npoints))
 
-    cnt = 0
-    for x in allpoints
-        if indomain(x...)
-            pointstate.x[cnt+=1] = x
+    if :x in propertynames(pointstate)
+        cnt = 0
+        for x in allpoints
+            if indomain(x...)
+                @inbounds pointstate.x[cnt+=1] = x
+            end
         end
     end
-
-    V = prod(h)
-    for i in 1:npoints
+    if :V0 in propertynames(pointstate)
+        V = prod(h)
         if dim == 2 && coord_system isa Axisymmetric
-            r = pointstate.x[i][1]
-            pointstate.V0[i] = r * V
+            @. pointstate.V0 = getindex(pointstate.x, 1) * V
         else
-            pointstate.V0[i] = V
+            @. pointstate.V0 = V
         end
-        pointstate.h[i] = Vec(h)
+    end
+    if :side_length in propertynames(pointstate)
+        pointstate.h .= Vec(h)
+    end
+    if :index in propertynames(pointstate)
+        pointstate.index .= 1:npoints
     end
 
     pointstate
 end
 
 function generate_pointstate(indomain, grid::Grid{dim, T}, coord_system::CoordinateSystem = DefaultCoordinateSystem(); n::Int = 2) where {dim, T}
-    generate_pointstate(indomain, @NamedTuple{x::Vec{dim, T}, V0::T, h::Vec{dim,T}}, grid, coord_system; n)
+    generate_pointstate(indomain, @NamedTuple{x::Vec{dim, T}, V0::T, side_length::Vec{dim, T}, index::Int}, grid, coord_system; n)
 end
