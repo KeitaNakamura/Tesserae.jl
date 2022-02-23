5177671dd6e8ec2510fd15a0b9ac76f02555419e
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-02-23 19:44:21 +0900

    Remove `DefaultNodeStateWLS` (#202)

--- src/nodestate.jl
+++ src/nodestate.jl
@@ -6,15 +6,4 @@ struct DefaultNodeState{dim, T, L, LL}
     poly_mat::Mat{L, L, T, LL}
 end
 
-default_nodestate_type(::Union{BSpline, KernelCorrection}, ::Val{dim}, ::Val{T}) where {dim, T} = DefaultNodeState{dim, T, dim+1, (dim+1)*(dim+1)}
-default_nodestate_type(::GIMP, ::Val{dim}, ::Val{T}) where {dim, T} = DefaultNodeState{dim, T, dim+1, (dim+1)*(dim+1)}
-
-struct DefaultNodeStateWLS{dim, T, L, LL}
-    m::T
-    v::Vec{dim, T}
-    v_n::Vec{dim, T}
-    poly_coef::Vec{L, T}
-    poly_mat::Mat{L, L, T, LL}
-end
-
-default_nodestate_type(::WLS, ::Val{dim}, ::Val{T}) where {dim, T} = DefaultNodeStateWLS{dim, T, dim+1, (dim+1)*(dim+1)}
+default_nodestate_type(::Interpolation, ::Val{dim}, ::Val{T}) where {dim, T} = DefaultNodeState{dim, T, dim+1, (dim+1)*(dim+1)}
