01414d3873f554320de4cab1bfddfa7fd3df85c8
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-02-23 18:57:01 +0900

    Modify tests to decrease execution time (#201)

--- test/runtests.jl
+++ test/runtests.jl
@@ -57,15 +57,19 @@ function check_example(testname::String, case, interpolation; dx, kwargs...)
 end
 
 @testset "Check examples" begin
-    check_example("sandcolumn", 1, QuadraticBSpline(); dx = 0.01)
-    check_example("sandcolumn", 2, LinearWLS(QuadraticBSpline()); dx = 0.01)
-    check_example("sandcolumn", 2, LinearWLS(QuadraticBSpline()); dx = 0.01, transfer = TransferTaylorPIC())
-    check_example("sandcolumn", 3, BilinearWLS(QuadraticBSpline()); dx = 0.01)
-    check_example("sandcolumn", 4, KernelCorrection(QuadraticBSpline()); dx = 0.01)
-    check_example("sandcolumn", 5, KernelCorrection(QuadraticBSpline()); dx = 0.01, transfer = TransferAffinePIC())
-    check_example("stripfooting", 1, LinearBSpline(); dx = 0.1, ν = 0.49, handle_volumetric_locking = true)
-    check_example("stripfooting", 2, GIMP(); dx = 0.1, ν = 0.49, handle_volumetric_locking = true)
-    check_example("stripfooting", 3, LinearWLS(QuadraticBSpline()); dx = 0.1, ν = 0.49, handle_volumetric_locking = true)
-    check_example("stripfooting", 4, KernelCorrection(QuadraticBSpline()); dx = 0.1, ν = 0.49, handle_volumetric_locking = true)
-    check_example("stripfooting", 5, KernelCorrection(QuadraticBSpline()); dx = 0.1, ν = 0.49, handle_volumetric_locking = true, transfer = TransferAffinePIC())
+    dx = 0.01
+    check_example("sandcolumn", 1, QuadraticBSpline(); dx)
+    check_example("sandcolumn", 2, LinearWLS(QuadraticBSpline()); dx)
+    check_example("sandcolumn", 2, LinearWLS(QuadraticBSpline()); dx, transfer = TransferTaylorPIC())
+    check_example("sandcolumn", 3, BilinearWLS(QuadraticBSpline()); dx)
+    check_example("sandcolumn", 4, KernelCorrection(QuadraticBSpline()); dx)
+    check_example("sandcolumn", 5, KernelCorrection(QuadraticBSpline()); dx, transfer = TransferAffinePIC())
+    dx = 0.125
+    ν = 0.3
+    handle_volumetric_locking = true
+    check_example("stripfooting", 1, LinearBSpline(); dx, ν, handle_volumetric_locking)
+    check_example("stripfooting", 2, GIMP(); dx, ν, handle_volumetric_locking)
+    check_example("stripfooting", 3, LinearWLS(QuadraticBSpline()); dx, ν, handle_volumetric_locking)
+    check_example("stripfooting", 4, KernelCorrection(QuadraticBSpline()); dx, ν, handle_volumetric_locking)
+    check_example("stripfooting", 5, KernelCorrection(QuadraticBSpline()); dx, ν, handle_volumetric_locking, transfer = TransferAffinePIC())
 end
