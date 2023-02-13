44c581d3d4fec9d79354a6c8ca35e22a3843237b
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-02-13 13:52:25 +0900

    Allow specifying state names to transfer (#329)

--- src/transfer.jl
+++ src/transfer.jl
@@ -19,17 +19,21 @@ const PICGroup = Union{PIC, APIC, TPIC}
 # helpers #
 ###########
 
-function check_grid_particles(grid::Grid, particles::AbstractVector, space::MPSpace)
+function check_particles(particles::AbstractVector, space::MPSpace)
     @assert length(particles) == num_particles(space)
-    @assert size(grid) == gridsize(space)
-    check_grid(grid, space)
 end
 
 function check_grid(grid::Grid, space::MPSpace)
+    @assert size(grid) == gridsize(space)
 end
-function check_grid(grid::SpGrid, space::MPSpace)
+function check_grid(grid::Union{SpGrid, SpArray}, space::MPSpace)
+    @assert size(grid) == gridsize(space)
     get_sppat(grid) !== get_gridsppat(space) &&
         error("Using different sparsity pattern between `MPSpace` and `Grid`")
+end
+
+function check_statenames(part::Tuple{Vararg{Symbol}}, all::Tuple{Vararg{Symbol}})
+    isempty(setdiff!(Set(part), Set(all))) || error("unsupported state names, got $part, available names are $all")
 end
 
 ################
@@ -38,79 +42,98 @@ end
 
 # default
 function transfer!(grid::Grid, particles::Particles, space::MPSpace, dt::Real; alg::TransferAlgorithm = DefaultTransfer(), system::CoordinateSystem = NormalSystem())
-    transfer!(alg, system, grid, particles, space, dt)
-end
-
-# don't use dispatch and all transfer algorithms are writtein in this function to reduce a lot of deplicated code
-function transfer!(alg::TransferAlgorithm, system::CoordinateSystem, grid::Grid, particles::Particles, space::MPSpace{dim, T}, dt::Real) where {dim, T}
-    check_grid_particles(grid, particles, space)
-
     fillzero!(grid.m)
     fillzero!(grid.vⁿ)
     fillzero!(grid.v)
+    transfer!(alg, system, (; m=grid.m, mv=grid.vⁿ, f=grid.v), particles, space)
+    @. grid.v = ((grid.vⁿ + dt*grid.v) / grid.m) * !iszero(grid.m)
+    @. grid.vⁿ = (grid.vⁿ / grid.m) * !iszero(grid.m)
+    grid
+end
 
+# don't use dispatch and all transfer algorithms are writtein in this function to reduce a lot of deplicated code
+function transfer!(alg::TransferAlgorithm, system::CoordinateSystem, grid::NamedTuple{names}, particles::Particles, space::MPSpace{dim, T}) where {names, dim, T}
+    check_statenames(names, (:m, :f, :mv))
+    :m  in names && check_grid(grid.m, space)
+    :f  in names && check_grid(grid.f, space)
+    :mv in names && check_grid(grid.mv, space)
+    check_particles(particles, space)
+
+    lattice = get_lattice(space)
     parallel_each_particle(space) do p
         @inbounds begin
             mp = mpvalue(space, p)
 
-            xₚ = particles.x[p]
+            # 100% used
             mₚ = particles.m[p]
-            Vₚ = particles.V[p]
-            σₚ = particles.σ[p]
-            bₚ = particles.b[p]
 
-            Vₚσₚ = Vₚ * σₚ
-            mₚbₚ = mₚ * bₚ
+            if :f in names
+                xₚ = particles.x[p]
+                Vₚ = particles.V[p]
+                σₚ = particles.σ[p]
+                bₚ = particles.b[p]
+                Vₚσₚ = Vₚ * σₚ
+                mₚbₚ = mₚ * bₚ
+            end
 
-            # grid momentum depends on transfer algorithm
-            if alg isa DefaultTransfer && mp isa WLSValue
-                Cₚ = particles.C[p]
-                mₚCₚ = mₚ * Cₚ
-                P = x -> value(get_basis(mp), x)
-            else
-                vₚ = particles.v[p]
-                mₚvₚ = mₚ * vₚ
-                if alg isa AffineGroup
-                    Bₚ = particles.B[p]
-                    Dₚ = zero(Mat{dim, dim, T})
-                    for (j, i) in enumerate(neighbornodes(space, p))
-                        xᵢ = grid.x[i]
-                        N = shape_value(mp, j)
-                        Dₚ += N*(xᵢ-xₚ)⊗(xᵢ-xₚ)
+            # grid momentum depends on transfer algorithms
+            if :mv in names
+                if alg isa DefaultTransfer && mp isa WLSValue
+                    P = x -> value(get_basis(mp), x)
+                    Cₚ = particles.C[p]
+                    mₚCₚ = mₚ * Cₚ
+                else
+                    vₚ = particles.v[p]
+                    mₚvₚ = mₚ * vₚ
+
+                    # additional term from high order approximation
+                    if alg isa AffineGroup
+                        Bₚ = particles.B[p]
+                        Dₚ = zero(Mat{dim, dim, T})
+                        for (j, i) in enumerate(neighbornodes(space, p))
+                            N = shape_value(mp, j)
+                            xᵢ = lattice[i]
+                            Dₚ += N*(xᵢ-xₚ)⊗(xᵢ-xₚ)
+                        end
+                        mₚCₚ = mₚ * Bₚ ⋅ inv(Dₚ)
+                    elseif alg isa TaylorGroup
+                        ∇vₚ = particles.∇v[p]
+                        mₚ∇vₚ = mₚ * @Tensor(∇vₚ[1:dim, 1:dim])
                     end
-                    mₚCₚ = mₚ * Bₚ ⋅ inv(Dₚ)
-                elseif alg isa TaylorGroup
-                    ∇vₚ = particles.∇v[p]
-                    mₚ∇vₚ = mₚ * @Tensor(∇vₚ[1:dim, 1:dim])
                 end
             end
 
             for (j, i) in enumerate(neighbornodes(space, p))
                 N = shape_value(mp, j)
                 ∇N = shape_gradient(mp, j)
-                f = -stress_to_force(system, N, ∇N, xₚ, Vₚσₚ) + N*mₚbₚ
-                grid.m[i] += N*mₚ
-                grid.v[i] += dt*f
 
-                # grid momentum depends on transfer algorithm
-                if alg isa DefaultTransfer && mp isa WLSValue
-                    xᵢ = grid.x[i]
-                    grid.vⁿ[i] += N*mₚCₚ⋅P(xᵢ-xₚ)
-                elseif alg isa AffineGroup
-                    xᵢ = grid.x[i]
-                    grid.vⁿ[i] += N*(mₚvₚ + mₚCₚ⋅(xᵢ-xₚ))
-                elseif alg isa TaylorGroup
-                    xᵢ = grid.x[i]
-                    grid.vⁿ[i] += N*(mₚvₚ + mₚ∇vₚ⋅(xᵢ-xₚ))
-                else
-                    grid.vⁿ[i] += N*mₚvₚ
+                if :m in names
+                    grid.m[i] += N*mₚ
+                end
+
+                if :f in names
+                    fint = -stress_to_force(system, N, ∇N, xₚ, Vₚσₚ)
+                    grid.f[i] += fint + N*mₚbₚ
+                end
+
+                # grid momentum depends on transfer algorithms
+                if :mv in names
+                    if alg isa DefaultTransfer && mp isa WLSValue
+                        xᵢ = lattice[i]
+                        grid.mv[i] += N*mₚCₚ⋅P(xᵢ-xₚ)
+                    elseif alg isa AffineGroup
+                        xᵢ = lattice[i]
+                        grid.mv[i] += N*(mₚvₚ + mₚCₚ⋅(xᵢ-xₚ))
+                    elseif alg isa TaylorGroup
+                        xᵢ = lattice[i]
+                        grid.mv[i] += N*(mₚvₚ + mₚ∇vₚ⋅(xᵢ-xₚ))
+                    else
+                        grid.mv[i] += N*mₚvₚ
+                    end
                 end
             end
         end
     end
-
-    @. grid.v = ((grid.vⁿ + grid.v) / grid.m) * !iszero(grid.m)
-    @. grid.vⁿ = (grid.vⁿ / grid.m) * !iszero(grid.m)
 
     grid
 end
@@ -131,55 +154,108 @@ end
 
 # default
 function transfer!(particles::Particles, grid::Grid, space::MPSpace, dt::Real; alg::TransferAlgorithm = DefaultTransfer(), system::CoordinateSystem = NormalSystem())
-    transfer!(alg, system, particles, grid, space, dt)
+    _transfer!(alg, system, particles, grid, space, dt)
+end
+function _transfer!(alg::TransferAlgorithm, system::CoordinateSystem, particles::Particles, grid::Grid, space::MPSpace, dt::Real)
+    transfer!(alg, system, (; ∇v=particles.∇v, x=particles.x, v=particles.v), grid, space, dt)
+end
+function _transfer!(alg::AffineGroup, system::CoordinateSystem, particles::Particles, grid::Grid, space::MPSpace, dt::Real)
+    transfer!(alg, system, (; ∇v=particles.∇v, x=particles.x, v=particles.v, B=particles.B), grid, space, dt)
+end
+function _transfer!(alg::DefaultTransfer, system::CoordinateSystem, particles::Particles, grid::Grid, space::MPSpace{<: Any, <: Any, <: WLSValue}, dt::Real)
+    transfer!(alg, system, (; ∇v=particles.∇v, x=particles.x, v=particles.v, C=particles.C), grid, space, dt)
 end
 
-function transfer!(alg::TransferAlgorithm, system::CoordinateSystem, particles::Particles, grid::Grid, space::MPSpace{dim}, dt::Real) where {dim}
-    check_grid_particles(grid, particles, space)
+function transfer!(alg::TransferAlgorithm, system::CoordinateSystem, particles::NamedTuple{names}, grid::Grid, space::MPSpace{dim}, dt::Real) where {names, dim}
+    check_statenames(names, (:∇v, :x, :v, :B))
+    check_grid(grid, space)
+    :∇v in names && check_particles(particles.∇v, space)
+    :x  in names && check_particles(particles.x, space)
+    :v  in names && check_particles(particles.v, space)
+    :B  in names && check_particles(particles.B, space) # for affine
 
     @threaded for p in 1:num_particles(space)
         mp = mpvalue(space, p)
 
+        # 100% used
         xₚ  = particles.x[p]
-        vₚ  = zero(eltype(particles.v))
-        ∇vₚ = @Tensor zero(eltype(particles.∇v))[1:dim, 1:dim]
 
-        if alg isa FLIPGroup
-            dvₚ = zero(eltype(particles.v))
-        end
-        if alg isa AffineGroup
-            Bₚ = zero(eltype(particles.B))
+        # there is no difference along with algorithms for calculating `:∇v` and `:x`
+        if :∇v in names
+            vₚ  = zero(eltype(particles.v))
+            ∇vₚ = @Tensor zero(eltype(particles.∇v))[1:dim, 1:dim]
         end
 
+        if :x in names
+            vₚ = zero(eltype(particles.v))
+        end
+
+        # particle velocity depends on transfer algorithms
+        if :v in names
+            if alg isa FLIPGroup
+                dvₚ = zero(eltype(particles.v))
+            else
+                @assert alg isa PICGroup
+                vₚ = zero(eltype(particles.v))
+            end
+            if alg isa AffineGroup
+                # Bₚ is always calculated when `:v` is specified
+                Bₚ = zero(eltype(particles.B))
+            end
+        end
+
+        lattice = get_lattice(space)
         for (j, i) in enumerate(neighbornodes(space, p))
             N = shape_value(mp, j)
             ∇N = shape_gradient(mp, j)
 
+            # 100% used
             vᵢ = grid.v[i]
-            vₚ  += vᵢ * N
-            ∇vₚ += vᵢ ⊗ ∇N
 
+            if :∇v in names
+                ∇vₚ += vᵢ ⊗ ∇N
+            end
+
+            # use `@isdefined` to avoid complicated check
+            # for `:v` in `PIC` is also calculated here
+            if @isdefined vₚ
+                vₚ += vᵢ * N
+            end
+
+            # particle velocity depends on transfer algorithms
+            if :v in names
+                if alg isa FLIPGroup
+                    dvᵢ = vᵢ - grid.vⁿ[i]
+                    dvₚ += N * dvᵢ
+                end
+                if alg isa AffineGroup
+                    xᵢ = lattice[i]
+                    Bₚ += N * vᵢ ⊗ (xᵢ - xₚ)
+                end
+            end
+        end
+
+        if :∇v in names
+            particles.∇v[p] = velocity_gradient(system, xₚ, vₚ, ∇vₚ)
+        end
+
+        if :x in names
+            particles.x[p] = xₚ + vₚ * dt
+        end
+
+        # particle velocity depends on transfer algorithms
+        if :v in names
             if alg isa FLIPGroup
-                dvᵢ = vᵢ - grid.vⁿ[i]
-                dvₚ += N * dvᵢ
+                particles.v[p] += dvₚ
+            else
+                @assert alg isa PICGroup
+                particles.v[p] = vₚ
             end
             if alg isa AffineGroup
-                xᵢ = grid.x[i]
-                Bₚ += N * vᵢ ⊗ (xᵢ - xₚ)
+                # additional quantity for affine transfers
+                # Bₚ is always calculated when `:v` is specified
+                particles.B[p] = Bₚ
             end
-        end
-
-        particles.x[p] += vₚ * dt
-        particles.∇v[p] = velocity_gradient(system, xₚ, vₚ, ∇vₚ)
-
-        if alg isa FLIPGroup
-            particles.v[p] += dvₚ
-        else
-            @assert alg isa PICGroup
-            particles.v[p] = vₚ
-        end
-        if alg isa AffineGroup
-            particles.B[p] = Bₚ
         end
     end
 
@@ -187,16 +263,21 @@ function transfer!(alg::TransferAlgorithm, system::CoordinateSystem, particles::
 end
 
 # special default transfer for `WLS` interpolation
-function transfer!(::DefaultTransfer, system::CoordinateSystem, particles::Particles, grid::Grid, space::MPSpace{dim, <: Any, <: WLSValue}, dt::Real) where {dim}
-    check_grid_particles(grid, particles, space)
+function transfer!(::DefaultTransfer, system::CoordinateSystem, particles::NamedTuple{names}, grid::Grid, space::MPSpace{dim, <: Any, <: WLSValue}, dt::Real) where {names, dim}
+    check_statenames(names, (:∇v, :x, :v, :C))
+    check_grid(grid, space)
+    :∇v in names && check_particles(particles.∇v, space)
+    :x  in names && check_particles(particles.x, space)
+    :v  in names && check_particles(particles.v, space)
+    :C  in names && check_particles(particles.C, space)
 
     @threaded for p in 1:num_particles(space)
+        mp = mpvalue(space, p)
+
         xₚ = particles.x[p]
         Cₚ = zero(eltype(particles.C))
-        mp = mpvalue(space, p)
         P = x -> value(get_basis(mp), x)
-        p0 = value(get_basis(mp), zero(Vec{dim, Int}))
-        ∇p0 = gradient(get_basis(mp), zero(Vec{dim, Int}))
+
         for (j, i) in enumerate(neighbornodes(space, p))
             w = mp.w[j]
             Minv = mp.Minv
@@ -204,11 +285,23 @@ function transfer!(::DefaultTransfer, system::CoordinateSystem, particles::Parti
             xᵢ = grid.x[i]
             Cₚ += vᵢ ⊗ (w * Minv ⋅ P(xᵢ - xₚ))
         end
+
+        p0 = value(get_basis(mp), zero(Vec{dim, Int}))
         vₚ = Cₚ ⋅ p0
-        particles.C[p] = Cₚ
-        particles.∇v[p] = velocity_gradient(system, xₚ, vₚ, Cₚ ⋅ ∇p0)
-        particles.v[p] = vₚ
-        particles.x[p] += vₚ * dt
+
+        if :∇v in names
+            ∇p0 = gradient(get_basis(mp), zero(Vec{dim, Int}))
+            particles.∇v[p] = velocity_gradient(system, xₚ, vₚ, Cₚ ⋅ ∇p0)
+        end
+
+        if :x in names
+            particles.x[p] = xₚ + vₚ * dt
+        end
+
+        if :v in names
+            particles.v[p] = vₚ
+            particles.C[p] = Cₚ # always update when velocity is updated
+        end
     end
 
     particles
@@ -239,8 +332,10 @@ end
 end
 
 function smooth_particle_state!(vals::AbstractVector, xₚ::AbstractVector, Vₚ::AbstractVector, grid::Grid, space::MPSpace)
-    check_grid_particles(grid, vals, space)
-    @assert length(vals) == length(xₚ) == length(Vₚ)
+    check_grid(grid, space)
+    check_particles(vals, space)
+    check_particles(xₚ, space)
+    check_particles(Vₚ, space)
 
     basis = PolynomialBasis{1}()
     fillzero!(grid.poly_coef)
