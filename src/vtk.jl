626d84d7cb7797b332cba2ce78dacd0bc2f4562b
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-03-23 21:44:53 +0900

    Extend 2d vector to 3d in vtk output to draw vector in Glyph

--- src/vtk.jl
+++ src/vtk.jl
@@ -1,10 +1,10 @@
+# vtk_grid
 function WriteVTK.vtk_grid(vtk::AbstractString, x::AbstractVector{<: Vec}; kwargs...)
     coords = vtk_format(x)
     npts = length(x)
     cells = [MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i in 1:npts]
     vtk_grid(vtk, coords, cells; kwargs...)
 end
-
 function WriteVTK.vtk_grid(vtk::AbstractString, lattice::Lattice; kwargs...)
     vtk_grid(vtk, map(collect, get_axes(lattice))...; kwargs...)
 end
@@ -12,17 +12,19 @@ function WriteVTK.vtk_grid(vtk::AbstractString, grid::Grid; kwargs...)
     vtk_grid(vtk, get_lattice(grid); kwargs...)
 end
 
-function WriteVTK.add_field_data(vtk::WriteVTK.DatasetFile, data::AbstractVector{<: Tensor}, name::AbstractString, ::WriteVTK.VTKPointData)
+# add_field_data
+function WriteVTK.add_field_data(vtk::WriteVTK.DatasetFile, data::AbstractArray{<: Tensor}, name::AbstractString, loc::WriteVTK.AbstractFieldData; kwargs...)
     vtk_point_data(vtk, vtk_format(data), name)
 end
-
-function vtk_format(x::AbstractVector{Vec{dim, T}}) where {dim, T}
-    reshape(reinterpret(T, x), (dim, length(x)))
+vtk_format(A::AbstractArray{<: Tensor}) = _vtk_format(A)
+function vtk_format(A::AbstractArray{<: Vec{2}})
+    _vtk_format(map(x->[x;0], A)) # extend to 3D to draw vector in Glyph
 end
-function vtk_format(data::AbstractVector{<: SymmetricSecondOrderTensor{3}})
-    vtk_format([@inbounds Vec(x[1,1], x[2,2], x[3,3], x[1,2], x[2,3], x[1,3]) for x in data])
+function _vtk_format(A::AbstractArray{<: Tensor})
+    reinterpret(reshape, eltype(eltype(A)), A)
 end
 
+# open/close
 openvtk(args...; kwargs...) = vtk_grid(args...; kwargs...)
 openvtm(args...; kwargs...) = vtk_multiblock(args...; kwargs...)
 openpvd(args...; kwargs...) = paraview_collection(args...; kwargs...)
