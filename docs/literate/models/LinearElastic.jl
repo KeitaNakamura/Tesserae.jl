e2a3cb9ee8532931179406e4a3474b005624d7c9
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-05-07 13:51:38 +0900

    Update LinearElastic

--- docs/literate/models/LinearElastic.jl
+++ docs/literate/models/LinearElastic.jl
@@ -13,13 +13,27 @@ struct LinearElastic{T}
     λ::T # Lame's first parameter
     μ::T # shear modulus
     c::SymmetricFourthOrderTensor{3, T, 36}
+    c_inv::SymmetricFourthOrderTensor{3, T, 36}
+    c33::Mat{3, 3, T, 9}
+    c33_inv::Mat{3, 3, T, 9}
 end
 
 function LinearElastic(; E::T, ν::T) where {T}
-    δ = one(SymmetricSecondOrderTensor{3})
-    I = one(SymmetricFourthOrderTensor{3})
     λ = (E*ν) / ((1+ν)*(1-2ν))
     μ = E / 2(1 + ν)
+
+    δ = one(SymmetricSecondOrderTensor{3})
+    I = one(SymmetricFourthOrderTensor{3})
     c = λ*δ⊗δ + 2μ*I
-    LinearElastic{T}(E, ν, λ, μ, c)
+
+    δ = ones(Vec{3})
+    I = one(Mat{3,3})
+    c33 = λ*δ⊗δ + 2μ*I
+    LinearElastic{T}(E, ν, λ, μ, c, inv(c), c33, inv(c33))
 end
+
+compute_stress(model::LinearElastic, ϵ::SymmetricSecondOrderTensor{3}) = model.c ⊡ ϵ
+compute_stress(model::LinearElastic, ϵ::Vec{3}) = model.c33 ⋅ ϵ
+
+compute_strain(model::LinearElastic, σ::SymmetricSecondOrderTensor{3}) = model.c_inv ⊡ σ
+compute_strain(model::LinearElastic, σ::Vec{3}) = model.c33_inv ⋅ σ
