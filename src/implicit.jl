0ef14b57377a0ed2ca6f6d3f0690b6c829e39805
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-06-10 22:00:04 +0900

    Implement recompute_grid_force for refactoring implicit method

--- src/implicit.jl
+++ src/implicit.jl
@@ -26,32 +26,38 @@ end
 isless_eps(x::Real, p::Int) = abs(x) < eps(typeof(x))^(1/p)
 isconverged(x::Real, solver::NewtonSolver) = abs(x) < solver.tol
 
-# for matrix-free linear solver
-function jacobian_matrix(particles::Particles, grid::Grid, space::MPSpace, Δt::Real, freedofs::Vector{<: CartesianIndex}; alg::TransferAlgorithm=FLIP(), system::CoordinateSystem=DefaultSystem(), parallel::Bool=true)
-    LinearMap(length(freedofs)) do Jδv, δv
+function recompute_grid_force!(update_stress!, grid::Grid, particles::Particles, space::MPSpace, alg::TransferAlgorithm, system::CoordinateSystem, parallel::Bool)
+    fillzero!(grid.f)
+    parallel_each_particle(space; parallel) do p
         @inbounds begin
-            # grid-to-particle to compute δvₚ from δvᵢ, then compute Cauchy stress increment
-            flatarray(fillzero!(grid.δv))[freedofs] .= δv
-            grid_to_particle!(:∇v, particles, @rename(grid, δv=>v, v=>_), space; alg, system, parallel) do pt
-                @_inline_meta
-                @inbounds begin
-                    δvₚ = pt.∇v
-                    pt.δσ = (pt.ℂ ⊡ δvₚ) / pt.V
-                end
-            end
-
-            # particle-to-grid to compute δfᵢ (i.e., ∂fᵢ∂δvᵢ ⋅ δvᵢ)
-            δf = fillzero!(grid.δv) # reuse grid.δv
-            particle_to_grid!(:f, @rename(grid, δv=>f, f=>_), @rename(particles, δσ=>σ, σ=>_), space; alg, system, parallel)
-
-            # compute J⋅δvᵢ (matrix-vector product)
-            δa = view(flatarray(δf ./= grid.m), freedofs)
-            @. Jδv = δv - Δt * δa
+            pt = LazyRow(particles, p)
+            itp = get_interpolation(space)
+            mp = values(space, p)
+            grid_to_particle!(alg, system, Val((:∇v,)), pt, grid, itp, mp)
+            update_stress!(pt)
+            particle_to_grid!(alg, system, Val((:f,)), grid, pt, itp, mp)
         end
     end
 end
 
-function diagonal_preconditioner!(P::AbstractVector, particles::Particles, grid::Grid{dim}, space::MPSpace{dim}, Δt::Real, freedofs::Vector{<: CartesianIndex}; parallel::Bool=true) where {dim}
+# for matrix-free linear solver
+function jacobian_matrix(particles::Particles, grid::Grid, space::MPSpace, Δt::Real, freedofs::Vector{<: CartesianIndex}, alg::TransferAlgorithm, system::CoordinateSystem, parallel::Bool)
+    LinearMap(length(freedofs)) do Jδv, δv
+        flatarray(fillzero!(grid.δv))[freedofs] .= δv
+        recompute_grid_force!(@rename(grid, δv=>v, δf=>f, v=>_v, f=>_f), @rename(particles, δσ=>σ, σ=>_σ), space, alg, system, parallel) do pt
+            @_inline_meta
+            @inbounds begin
+                δvₚ = pt.∇v
+                pt.σ = (pt.ℂ ⊡ δvₚ) / pt.V
+            end
+        end
+        # compute J⋅δvᵢ (matrix-vector product)
+        δa = view(flatarray(grid.δf ./= grid.m), freedofs)
+        @. Jδv = δv - Δt * δa
+    end
+end
+
+function diagonal_preconditioner!(P::AbstractVector, particles::Particles, grid::Grid{dim}, space::MPSpace{dim}, Δt::Real, freedofs::Vector{<: CartesianIndex}, parallel::Bool) where {dim}
     fill!(P, 1)
     parallel_each_particle(space; parallel) do p
         @_inline_meta
@@ -81,7 +87,8 @@ end
 function grid_to_particle!(update_stress!, alg::TransferAlgorithm, system::CoordinateSystem, ::Val{(:∇v,)}, particles::Particles, grid::Grid{dim}, space::MPSpace{dim}, Δt::Real, solver::NewtonSolver{T}, isfixed::AbstractArray{Bool}; parallel::Bool) where {dim, T}
     @assert size(isfixed) == (dim, size(grid)...)
 
-    grid_to_particle!(update_stress!, :∇v, particles, grid, space; alg, system, parallel)
+    # recompute particle stress and grid force
+    recompute_grid_force!(update_stress!, grid, particles, space, alg, system, parallel)
 
     if solver.maxiter != 0
         freedofs = filter(CartesianIndices(isfixed)) do I
@@ -90,37 +97,32 @@ function grid_to_particle!(update_stress!, alg::TransferAlgorithm, system::Coord
         end
 
         resize!(solver, length(freedofs))
-        A = jacobian_matrix(particles, grid, space, Δt, freedofs; alg, system, parallel)
+        A = jacobian_matrix(particles, grid, space, Δt, freedofs, alg, system, parallel)
 
         vⁿ = @inbounds view(flatarray(grid.vⁿ), freedofs)
         if !isless_eps(maximum(abs, vⁿ), 1)
-            ok = false
             r⁰ = norm(vⁿ)
             @inbounds for k in 1:solver.maxiter
-                # compute grid force at k iterations
-                fillzero!(grid.f)
-                particle_to_grid!(:f, grid, particles, space; alg, system, parallel)
-
                 # compute residual for Newton's method
                 R = grid.δv # reuse grid.δv
                 @. R = grid.v - grid.vⁿ - Δt * (grid.f/grid.m)
                 solver.R .= view(flatarray(R), freedofs)
-                isconverged(norm(solver.R)/r⁰, solver) && (ok=true; break)
+                isconverged(norm(solver.R)/r⁰, solver) && return
 
                 # solve linear equation A⋅δv = -R
-                # P = diagonal_preconditioner!(solver.P, particles, grid, space, Δt, freedofs; parallel)
+                # P = diagonal_preconditioner!(solver.P, particles, grid, space, Δt, freedofs, parallel)
                 # solver.linsolve(solver.δv, A, rmul!(solver.R, -1); Pl=P)
                 solver.linsolve(solver.δv, A, rmul!(solver.R, -1))
-                isconverged(norm(solver.δv), solver) && (ok=true; break)
+                isconverged(norm(solver.δv), solver) && return
 
                 # update grid velocity
                 v = view(flatarray(grid.v), freedofs)
                 @. v += solver.δv
 
-                # recompute particle stress from grid velocity
-                grid_to_particle!(update_stress!, :∇v, particles, grid, space; alg, system, parallel)
+                # recompute particle stress and grid force
+                recompute_grid_force!(update_stress!, grid, particles, space, alg, system, parallel)
             end
-            ok || @warn "Newton's method not converged"
+            @warn "Newton's method not converged"
         end
     end
 end
