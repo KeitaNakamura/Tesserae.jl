6a010acf356e1a049b4f9a145513a24cf7547ef7
Keita Nakamura <keita.nakamura.1109@gmail.com>
2024-02-17 14:23:06 +0900

    Add example for TLMPM

--- docs/literate.jl
+++ docs/literate.jl
@@ -1,6 +1,7 @@
 using Literate
 
-const EXAMPLES = ["elastic_impact.jl"]
+const EXAMPLES = ["elastic_impact.jl",
+                  "tlmpm_vortex.jl"]
 
 for filename in EXAMPLES
     path = joinpath(@__DIR__, "literate/examples", filename)
--- test/examples.jl
+++ test/examples.jl
@@ -6,4 +6,9 @@ const RUN_TESTS = true
             include(joinpath(@__DIR__, "../docs/literate/examples/elastic_impact.jl"))
         end
     end
+    @testset "Vortex by Total Lagrangian MPM" begin
+        cd(tempdir()) do
+            include(joinpath(@__DIR__, "../docs/literate/examples/tlmpm_vortex.jl"))
+        end
+    end
 end
