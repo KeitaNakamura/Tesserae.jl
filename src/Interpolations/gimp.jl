16db23ba24d4363485d56f3b22a3d75e6708d993
Keita Nakamura <keita.nakamura.1109@gmail.com>
2024-08-21 11:52:59 +0900

    Update docs for uGIMP

--- src/Interpolations/gimp.jl
+++ src/Interpolations/gimp.jl
@@ -1,13 +1,20 @@
 """
     uGIMP()
 
-The unchanged GIMP (generalized interpolation material point) kernel [^GIMP].
+A kernel for the unchanged generalized interpolation material point (uGIMP) [^GIMP].
+`uGIMP` requires the initial particle length `l` in the particle property as follows:
+
+```jl
+ParticleProp = @NamedTuple begin
+    < variables... >
+    l :: Float64
+end
+```
 
 [^GIMP]: [Bardenhagen, S. G., & Kober, E. M. (2004). The generalized interpolation material point method. *Computer Modeling in Engineering and Sciences*, 5(6), 477-496.](https://doi.org/10.3970/cmes.2004.005.477)
 """
 struct uGIMP <: Kernel end
 
-get_kernel(k::uGIMP) = k
 gridspan(::uGIMP) = 3
 
 @inline function neighboringnodes(::uGIMP, pt, mesh::CartesianMesh)
