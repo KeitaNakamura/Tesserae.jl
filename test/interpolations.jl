b6812c5de3bcdd15f71ccc8e78509257774e4e7d
Keita Nakamura <keita.nakamura.1109@gmail.com>
2024-08-24 12:33:47 +0900

    Update tests for interpolations

--- test/interpolations.jl
+++ test/interpolations.jl
@@ -1,40 +1,78 @@
 @testset "MPValue" begin
-    @testset "CartesianMesh" begin
-        for dim in (1,2,3)
-            mesh = CartesianMesh(1, ntuple(d->(0,10), dim)...)
-            for T in (Float32, Float64)
-                for kernel in (BSpline(Linear()), BSpline(Quadratic()), BSpline(Cubic()), SteffenBSpline(Linear()), SteffenBSpline(Quadratic()), SteffenBSpline(Cubic()), uGIMP())
-                    for extension in (identity, KernelCorrection)
-                        it = extension(kernel)
-                        mp = @inferred MPValue(T, it, mesh)
-                        @test Tesserae.interpolation(mp) === it
-                        @test mp.w isa Array{T}
-                        @test mp.∇w isa Array{Vec{dim,T}}
-                        @test ndims(mp.w) == dim
-                        @test ndims(mp.∇w) == dim
-                        @test size(mp.w) == size(mp.∇w)
-                        @test typeof(neighboringnodes(mp)) === CartesianIndices{dim, NTuple{dim, UnitRange{Int}}}
 
-                        # derivative = Order(0)
-                        mp = @inferred MPValue(T, it, mesh; derivative=Order(0))
-                        @test hasproperty(mp, :w) && mp.w isa Array{T}
-                        @test !hasproperty(mp, :∇w)
-                        @test !hasproperty(mp, :∇∇w)
-                        @test ndims(mp.w) == dim
-                        # derivative = Order(1)
-                        mp = @inferred MPValue(T, it, mesh; derivative=Order(1))
-                        @test hasproperty(mp, :w)  && mp.w  isa Array{T}
-                        @test hasproperty(mp, :∇w) && mp.∇w isa Array{Vec{dim,T}}
-                        @test !hasproperty(mp, :∇∇w)
-                        @test ndims(mp.w) == ndims(mp.∇w) == dim
-                        @test size(mp.w) == size(mp.∇w)
-                        # derivative = Order(2)
-                        mp = @inferred MPValue(T, it, mesh; derivative=Order(2))
-                        @test hasproperty(mp, :w)   && mp.w   isa Array{T}
-                        @test hasproperty(mp, :∇w)  && mp.∇w  isa Array{Vec{dim,T}}
-                        @test hasproperty(mp, :∇∇w) && mp.∇∇w isa Array{<: SymmetricSecondOrderTensor{dim,T}}
-                        @test ndims(mp.w) == ndims(mp.∇w) == ndims(mp.∇∇w) == dim
-                        @test size(mp.w) == size(mp.∇w) == size(mp.∇∇w)
+@testset "CartesianMesh" begin
+    for dim in (1,2,3)
+        mesh = CartesianMesh(1, ntuple(d->(0,10), dim)...)
+        for T in (Float32, Float64)
+            for kernel in (BSpline(Linear()), BSpline(Quadratic()), BSpline(Cubic()), SteffenBSpline(Linear()), SteffenBSpline(Quadratic()), SteffenBSpline(Cubic()), uGIMP())
+                for extension in (identity, WLS, KernelCorrection)
+                    it = extension(kernel)
+
+                    function check_mpvalue(mp::Union{MPValue, Tesserae.MPValueVector}, derivative)
+                        if derivative isa Order{0}
+                            @test hasproperty(mp, :w) && mp.w isa AbstractArray{T}
+                            @test !hasproperty(mp, :∇w)
+                            @test !hasproperty(mp, :∇∇w)
+                            @test ndims(mp.w) == ifelse(mp isa MPValue, dim, dim+1)
+                        elseif derivative isa Order{1}
+                            @test hasproperty(mp, :w)  && mp.w  isa AbstractArray{T}
+                            @test hasproperty(mp, :∇w) && mp.∇w isa AbstractArray{Vec{dim,T}}
+                            @test !hasproperty(mp, :∇∇w)
+                            @test size(mp.w) == size(mp.∇w)
+                            @test ndims(mp.w) == ndims(mp.∇w) == ifelse(mp isa MPValue, dim, dim+1)
+                        elseif derivative isa Order{2}
+                            @test hasproperty(mp, :w)   && mp.w   isa AbstractArray{T}
+                            @test hasproperty(mp, :∇w)  && mp.∇w  isa AbstractArray{Vec{dim,T}}
+                            @test hasproperty(mp, :∇∇w) && mp.∇∇w isa AbstractArray{<: SymmetricSecondOrderTensor{dim,T}}
+                            @test size(mp.w) == size(mp.∇w) == size(mp.∇∇w)
+                            @test ndims(mp.w) == ndims(mp.∇w) == ndims(mp.∇∇w) == ifelse(mp isa MPValue, dim, dim+1)
+                        elseif derivative isa Order{3}
+                            @test hasproperty(mp, :w)    && mp.w    isa AbstractArray{T}
+                            @test hasproperty(mp, :∇w)   && mp.∇w   isa AbstractArray{Vec{dim,T}}
+                            @test hasproperty(mp, :∇∇w)  && mp.∇∇w  isa AbstractArray{<: SymmetricSecondOrderTensor{dim,T}}
+                            @test hasproperty(mp, :∇∇∇w) && mp.∇∇∇w isa AbstractArray{<: Tensor{Tuple{@Symmetry{dim,dim,dim}},T}}
+                            @test size(mp.w) == size(mp.∇w) == size(mp.∇∇w) == size(mp.∇∇∇w)
+                            @test ndims(mp.w) == ndims(mp.∇w) == ndims(mp.∇∇w) == ndims(mp.∇∇∇w) == ifelse(mp isa MPValue, dim, dim+1)
+                        else
+                            error()
+                        end
+                    end
+
+                    ###########
+                    # MPValue #
+                    ###########
+                    mp = @inferred MPValue(T, it, mesh)
+                    @test Tesserae.interpolation(mp) === it
+                    @test mp.w isa Array{T}
+                    @test mp.∇w isa Array{Vec{dim,T}}
+                    @test ndims(mp.w) == dim
+                    @test ndims(mp.∇w) == dim
+                    @test size(mp.w) == size(mp.∇w)
+                    @test typeof(neighboringnodes(mp)) === CartesianIndices{dim, NTuple{dim, UnitRange{Int}}}
+                    check_mpvalue((@inferred MPValue(T, it, mesh; derivative=Order(0))), Order(0))
+                    check_mpvalue((@inferred MPValue(T, it, mesh; derivative=Order(1))), Order(1))
+                    check_mpvalue((@inferred MPValue(T, it, mesh; derivative=Order(2))), Order(2))
+                    check_mpvalue((@inferred MPValue(T, it, mesh; derivative=Order(3))), Order(3))
+
+                    #################
+                    # MPValueVector #
+                    #################
+                    n = 10
+                    mpvalues = @inferred generate_mpvalues(T, it, mesh, n)
+                    @test size(mpvalues) === (n,)
+                    @test Tesserae.interpolation(mpvalues) === it
+                    @test mpvalues.w isa Array{T}
+                    @test mpvalues.∇w isa Array{Vec{dim,T}}
+                    @test ndims(mpvalues.w) == dim+1
+                    @test ndims(mpvalues.∇w) == dim+1
+                    @test size(mpvalues.w) === size(mpvalues.∇w)
+                    @test all(eachindex(mpvalues)) do i
+                        typeof(mpvalues[i]) === eltype(mpvalues)
+                    end
+                    for order in 0:3
+                        mpvalues = @inferred generate_mpvalues(T, it, mesh, n; derivative=Order(order))
+                        check_mpvalue(mpvalues, Order(order))
+                        foreach(mp -> check_mpvalue(mp, Order(order)), mpvalues)
                     end
                 end
             end
@@ -42,25 +80,11 @@
     end
 end
 
-@testset "MPValueVector" begin
-    @testset "CartesianMesh" begin
-        for dim in (1,2,3)
-            mesh = CartesianMesh(1, ntuple(d->(0,10), dim)...)
-            for T in (Float32, Float64)
-                n = 100
-                mpvalues = @inferred generate_mpvalues(T, BSpline(Quadratic()), mesh, n)
-                @test size(mpvalues) === (n,)
-                @test Tesserae.interpolation(mpvalues) === BSpline(Quadratic())
-                @test all(eachindex(mpvalues)) do i
-                    typeof(mpvalues[i]) === eltype(mpvalues)
-                end
-            end
-        end
-    end
-end
+end # MPValue
 
 @testset "Interpolations" begin
 
+@testset "Check `update!` for `MPValue`" begin
     isapproxzero(x) = x + ones(x) ≈ ones(x)
     function check_partition_of_unity(mp, x; atol=sqrt(eps(eltype(mp.w))))
         indices = neighboringnodes(mp)
@@ -211,7 +235,7 @@ end
         mp = MPValue(KernelCorrection(kernel, poly), mesh)
         L = kernel isa BSpline{Quadratic} ? 1.5 :
             kernel isa BSpline{Cubic}     ? 2.0 : error()
-        X = ntuple(i -> range(max(mesh[1][i],index[i]-L-1), min(mesh[end][i],index[i]+L-1)-sqrt(eps(Float64)), step=0.04), Val(dim))
+        X = ntuple(i -> range(max(mesh[1][i],index[i]-L-1), min(mesh[end][i],index[i]+L-1)-sqrt(eps(Float64)), step=1/11), Val(dim)) # 1/10 is too coarse for checking
         Z = Array{Float64}(undef, length.(X))
         for i in CartesianIndices(Z)
             @inbounds Z[i] = kernelvalue(mp, Vec(map(getindex, X, Tuple(i))), mesh, index)
@@ -237,7 +261,7 @@ end
                 end
                 @test all(ispositive, kernelvalues(mesh, kern, multilin, i))
             end
-            # greedy kernel correction (only for hyperrectangle)
+            # aggressive kernel correction (only for hyperrectangle)
             for I in CartesianIndices((3,3))
                 @test !all(i -> all(ispositive, kernelvalues(CartesianMesh(1, (0,I[1]), (0,I[2])), kern, lin,      i)), CartesianIndices(Tuple(I)))
                 @test  all(i -> all(ispositive, kernelvalues(CartesianMesh(1, (0,I[1]), (0,I[2])), kern, multilin, i)), CartesianIndices(Tuple(I)))
@@ -254,7 +278,7 @@ end
                 end
                 @test all(ispositive, kernelvalues(mesh, kern, multilin, i))
             end
-            # greedy kernel correction (only for hyperrectangle)
+            # aggressive kernel correction (only for hyperrectangle)
             for I in CartesianIndices((3,3,3))
                 @test !all(i -> all(ispositive, kernelvalues(CartesianMesh(1, (0,I[1]), (0,I[2]), (0,I[3])), kern, lin,      i)), CartesianIndices(Tuple(I)))
                 @test  all(i -> all(ispositive, kernelvalues(CartesianMesh(1, (0,I[1]), (0,I[2]), (0,I[3])), kern, multilin, i)), CartesianIndices(Tuple(I)))
@@ -281,7 +305,7 @@ end
                     @test all(ispositive, kernelvalues(mesh, kern, multilin, i))
                 end
             end
-            # greedy kernel correction (only for hyperrectangle)
+            # aggressive kernel correction (only for hyperrectangle)
             for I in CartesianIndices((4,4))
                 @test !all(i -> all(ispositive, kernelvalues(CartesianMesh(1, (0,I[1]), (0,I[2])), kern, lin, i)), CartesianIndices(Tuple(I)))
                 if I == CartesianIndex(1,1)
@@ -307,7 +331,7 @@ end
                     @test all(ispositive, kernelvalues(mesh, kern, multilin, i))
                 end
             end
-            # greedy kernel correction (only for hyperrectangle)
+            # aggressive kernel correction (only for hyperrectangle)
             for I in CartesianIndices((4,4,4))
                 @test !all(i -> all(ispositive, kernelvalues(CartesianMesh(1, (0,I[1]), (0,I[2]), (0,I[3])), kern, lin, i)), CartesianIndices(Tuple(I)))
                 if I == CartesianIndex(1,1,1)
@@ -319,3 +343,5 @@ end
         end
     end
 end
+
+end # "Interpolations"
