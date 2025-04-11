fdc56071ff43caf0ee3a6bcb12fe31d5954074a3
Keita Nakamura <keita.nakamura.1109@gmail.com>
2025-04-11 16:59:43 +0900

    Add Quad8 and Hex20 (#27)

--- src/vtk.jl
+++ src/vtk.jl
@@ -6,10 +6,12 @@ to_vtk_celltype(::Line4) = WriteVTK.VTKCellTypes.VTK_CUBIC_LINE
 to_vtk_celltype(::Tri3)  = WriteVTK.VTKCellTypes.VTK_TRIANGLE
 to_vtk_celltype(::Tri6)  = WriteVTK.VTKCellTypes.VTK_QUADRATIC_TRIANGLE
 to_vtk_celltype(::Quad4) = WriteVTK.VTKCellTypes.VTK_QUAD
+to_vtk_celltype(::Quad8) = WriteVTK.VTKCellTypes.VTK_QUADRATIC_QUAD
 to_vtk_celltype(::Quad9) = WriteVTK.VTKCellTypes.VTK_BIQUADRATIC_QUAD
 to_vtk_celltype(::Tet4)  = WriteVTK.VTKCellTypes.VTK_TETRA
 to_vtk_celltype(::Tet10) = WriteVTK.VTKCellTypes.VTK_QUADRATIC_TETRA
 to_vtk_celltype(::Hex8)  = WriteVTK.VTKCellTypes.VTK_HEXAHEDRON
+to_vtk_celltype(::Hex20) = WriteVTK.VTKCellTypes.VTK_QUADRATIC_HEXAHEDRON
 to_vtk_celltype(::Hex27) = WriteVTK.VTKCellTypes.VTK_TRIQUADRATIC_HEXAHEDRON
 
 to_vtk_connectivity(::Line2) = SVector(1,2)
@@ -18,10 +20,12 @@ to_vtk_connectivity(::Line4) = SVector(1,2,3,4)
 to_vtk_connectivity(::Tri3)  = SVector(1,2,3)
 to_vtk_connectivity(::Tri6)  = SVector(1,2,3,4,6,5)
 to_vtk_connectivity(::Quad4) = SVector(1,2,3,4)
+to_vtk_connectivity(::Quad8) = SVector(1,2,3,4,5,6,7,8)
 to_vtk_connectivity(::Quad9) = SVector(1,2,3,4,5,6,7,8,9)
 to_vtk_connectivity(::Tet4)  = SVector(1,2,3,4)
 to_vtk_connectivity(::Tet10) = SVector(1,2,3,4,5,8,6,7,10,9)
 to_vtk_connectivity(::Hex8)  = SVector(1,2,3,4,5,6,7,8)
+to_vtk_connectivity(::Hex20) = SVector(1,2,3,4,5,6,7,8,9,12,14,10,17,19,20,18,11,13,15,16)
 to_vtk_connectivity(::Hex27) = SVector(1,2,3,4,5,6,7,8,9,12,14,10,17,19,20,18,11,13,15,16,23,24,22,25,21,26,27)
 
 # vtk_grid
