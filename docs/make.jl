442340a87112e27810e13fb77adcadc5abcbc55f
Keita Nakamura <keita.nakamura.1109@gmail.com>
2025-06-26 16:55:18 +0900

    Add an example Taylor imapct

--- docs/make.jl
+++ docs/make.jl
@@ -31,6 +31,7 @@ makedocs(;
             "tutorials/collapse.md",
             "Incompressible fluid flow" => "tutorials/dam_break.md",
             "tutorials/rigid_body_contact.md",
+            "tutorials/taylor_impact.md",
             "FEM (experimental)" => [
                 "tutorials/heat.md"
             ],
