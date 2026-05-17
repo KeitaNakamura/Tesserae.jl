dca97e94a225d4953f28a0f34be223257bdfdf0e
Keita Nakamura <keita.nakamura.1109@gmail.com>
2026-05-18 07:24:32 +0900

    Refactor transfer macro lowering (#107)

---
+++ test/transfer.jl
@@ -0,0 +1,231 @@
+@testset "Transfer macros" begin
+    function transfer_fixture()
+        mesh = CartesianMesh(1.0, (0,2), (0,2))
+        GridProp = @NamedTuple begin
+            x   :: Vec{2, Float64}
+            m   :: Float64
+            m⁻¹ :: Float64
+            mv  :: Vec{2, Float64}
+            f   :: Vec{2, Float64}
+            v   :: Vec{2, Float64}
+            vⁿ  :: Vec{2, Float64}
+        end
+        ParticleProp = @NamedTuple begin
+            x  :: Vec{2, Float64}
+            m  :: Float64
+            V  :: Float64
+            v  :: Vec{2, Float64}
+            ∇v :: SecondOrderTensor{2, Float64, 4}
+            F  :: SecondOrderTensor{2, Float64, 4}
+            σ  :: SecondOrderTensor{2, Float64, 4}
+        end
+
+        grid = generate_grid(GridProp, mesh)
+        particles = generate_particles(ParticleProp, mesh; alg=GridSampling())
+        weights = generate_basis_weights(BSpline(Linear()), mesh, length(particles))
+        update!(weights, particles, mesh)
+        initialize_mpm_state!(grid, particles)
+        grid, particles, weights
+    end
+
+    function initialize_mpm_state!(grid, particles)
+        for p in eachindex(particles)
+            particles.m[p] = 1.0 + 0.05p
+            particles.V[p] = 0.2 + 0.01p
+            particles.v[p] = Vec(0.15 + 0.02p, -0.25 + 0.015p)
+            particles.∇v[p] = zero(eltype(particles.∇v))
+            particles.F[p] = diagm(Vec(1.0 + 0.002p, 1.0 - 0.001p))
+            particles.σ[p] = symmetric(Vec(1.0 + 0.03p, 0.2) ⊗ Vec(0.4, 0.8 + 0.01p))
+        end
+
+        for i in eachindex(grid)
+            I = Tuple(i)
+            grid.m[i] = 1.0
+            grid.m⁻¹[i] = 1.0
+            grid.mv[i] = Vec(0.0, 0.0)
+            grid.f[i] = Vec(0.0, 0.0)
+            grid.vⁿ[i] = Vec(0.08 * I[1], -0.04 * I[2])
+            grid.v[i] = grid.vⁿ[i] + Vec(0.03 * I[2], -0.02 * I[1])
+        end
+
+        nothing
+    end
+
+    function manual_p2g!(grid, particles, weights, Δt, gravity)
+        fillzero!(grid.m)
+        fillzero!(grid.mv)
+        fillzero!(grid.f)
+
+        for p in eachindex(particles)
+            bw = weights[p]
+            nodeindices = supportnodes(bw, grid)
+            for ip in eachindex(nodeindices)
+                i = nodeindices[ip]
+                grid.m[i] += bw.w[ip] * particles.m[p]
+                grid.mv[i] += bw.w[ip] * particles.m[p] * particles.v[p]
+                grid.f[i] += bw.w[ip] * particles.m[p] * gravity
+                grid.f[i] -= particles.V[p] * particles.σ[p] * bw.∇w[ip]
+            end
+        end
+
+        for i in eachindex(grid)
+            grid.m⁻¹[i] = inv(grid.m[i]) * !iszero(grid.m[i])
+            grid.vⁿ[i] = grid.mv[i] * grid.m⁻¹[i]
+            grid.v[i] = grid.vⁿ[i] + (grid.f[i] * grid.m⁻¹[i]) * Δt
+        end
+
+        grid
+    end
+
+    function manual_g2p_pic_flip!(grid, particles, weights, α, Δt)
+        for p in eachindex(particles)
+            v_pic = zero(eltype(particles.v))
+            Δv_flip = zero(eltype(particles.v))
+            ∇v = zero(eltype(particles.∇v))
+            Δx = zero(eltype(particles.x))
+            bw = weights[p]
+            nodeindices = supportnodes(bw, grid)
+            for ip in eachindex(nodeindices)
+                i = nodeindices[ip]
+                v_pic += bw.w[ip] * grid.v[i]
+                Δv_flip += bw.w[ip] * (grid.v[i] - grid.vⁿ[i])
+                ∇v += grid.v[i] ⊗ bw.∇w[ip]
+                Δx += bw.w[ip] * grid.v[i] * Δt
+            end
+            particles.v[p] = (1 - α) * v_pic + α * (particles.v[p] + Δv_flip)
+            particles.∇v[p] = ∇v
+            particles.x[p] += Δx
+            particles.F[p] = (one(particles.F[p]) + particles.∇v[p] * Δt) * particles.F[p]
+        end
+
+        particles
+    end
+
+    function manual_g2p2g_internal_force!(grid, particles, weights, Δt, stiffness)
+        for p in eachindex(particles)
+            ∇v = zero(eltype(particles.∇v))
+            bw = weights[p]
+            nodeindices = supportnodes(bw, grid)
+            for ip in eachindex(nodeindices)
+                i = nodeindices[ip]
+                ∇v += grid.v[i] ⊗ bw.∇w[ip]
+            end
+            particles.∇v[p] = ∇v
+            particles.F[p] = (one(particles.F[p]) + particles.∇v[p] * Δt) * particles.F[p]
+            particles.σ[p] = stiffness * symmetric(particles.∇v[p])
+        end
+
+        fillzero!(grid.f)
+        for p in eachindex(particles)
+            bw = weights[p]
+            nodeindices = supportnodes(bw, grid)
+            for ip in eachindex(nodeindices)
+                i = nodeindices[ip]
+                grid.f[i] -= particles.V[p] * particles.σ[p] * bw.∇w[ip]
+            end
+        end
+
+        for i in eachindex(grid)
+            grid.v[i] = grid.vⁿ[i] + (grid.f[i] * grid.m⁻¹[i]) * Δt
+        end
+
+        grid, particles
+    end
+
+    @testset "@P2G" begin
+        Δt = 0.01
+        gravity = Vec(0.0, -9.81)
+        grid, particles, weights = transfer_fixture()
+        expected = deepcopy(grid)
+        actual = deepcopy(grid)
+
+        manual_p2g!(expected, particles, weights, Δt, gravity)
+
+        @P2G actual=>i particles=>p weights=>ip begin
+            m[i] = @∑ w[ip] * m[p]
+            mv[i] = @∑ w[ip] * m[p] * v[p]
+            f[i] = @∑ w[ip] * m[p] * gravity
+            f[i] -= @∑ V[p] * σ[p] * ∇w[ip]
+            m⁻¹[i] = inv(m[i]) * !iszero(m[i])
+            vⁿ[i] = mv[i] * m⁻¹[i]
+            v[i] = vⁿ[i] + (f[i] * m⁻¹[i]) * Δt
+        end
+
+        @test actual.m ≈ expected.m
+        @test actual.m⁻¹ ≈ expected.m⁻¹
+        @test actual.mv ≈ expected.mv
+        @test actual.f ≈ expected.f
+        @test actual.vⁿ ≈ expected.vⁿ
+        @test actual.v ≈ expected.v
+    end
+
+    @testset "@G2P" begin
+        α = 0.95
+        Δt = 0.01
+        gravity = Vec(0.0, -9.81)
+        grid, particles, weights = transfer_fixture()
+        manual_p2g!(grid, particles, weights, Δt, gravity)
+        expected = deepcopy(particles)
+        actual = deepcopy(particles)
+
+        manual_g2p_pic_flip!(grid, expected, weights, α, Δt)
+
+        @G2P grid=>i actual=>p weights=>ip begin
+            v[p] = @∑ w[ip] * ((1 - α) * v[i] + α * (v[p] + (v[i] - vⁿ[i])))
+            ∇v[p] = @∑ v[i] ⊗ ∇w[ip]
+            x[p] += @∑ w[ip] * v[i] * Δt
+            F[p] = (one(F[p]) + ∇v[p] * Δt) * F[p]
+        end
+
+        @test actual.v ≈ expected.v
+        @test actual.∇v ≈ expected.∇v
+        @test actual.x ≈ expected.x
+        @test actual.F ≈ expected.F
+    end
+
+    @testset "@G2P2G" begin
+        Δt = 0.01
+        stiffness = 2.5
+        gravity = Vec(0.0, -9.81)
+        grid, particles, weights = transfer_fixture()
+        manual_p2g!(grid, particles, weights, Δt, gravity)
+        expected_grid = deepcopy(grid)
+        expected_particles = deepcopy(particles)
+        actual_grid = deepcopy(grid)
+        actual_particles = deepcopy(particles)
+
+        manual_g2p2g_internal_force!(expected_grid, expected_particles, weights, Δt, stiffness)
+
+        @G2P2G actual_grid=>i actual_particles=>p weights=>ip begin
+            ∇v[p] = @∑ v[i] ⊗ ∇w[ip]
+            F[p] = (one(F[p]) + ∇v[p] * Δt) * F[p]
+            σ[p] = stiffness * symmetric(∇v[p])
+            f[i] = @∑ -V[p] * σ[p] * ∇w[ip]
+            v[i] = vⁿ[i] + (f[i] * m⁻¹[i]) * Δt
+        end
+
+        @test actual_particles.∇v ≈ expected_particles.∇v
+        @test actual_particles.F ≈ expected_particles.F
+        @test actual_particles.σ ≈ expected_particles.σ
+        @test actual_grid.f ≈ expected_grid.f
+        @test actual_grid.v ≈ expected_grid.v
+    end
+
+    @testset "ordering errors" begin
+        ex = quote
+            @P2G grid=>i particles=>p weights=>ip begin
+                v[i] = mv[i] * m⁻¹[i]
+                m[i] = @∑ w[ip] * m[p]
+            end
+        end
+        @test_throws ErrorException macroexpand(@__MODULE__, ex)
+
+        ex = quote
+            @G2P2G grid=>i particles=>p weights=>ip begin
+                f[i] = @∑ -V[p] * σ[p] * ∇w[ip]
+                ∇v[p] = @∑ v[i] ⊗ ∇w[ip]
+            end
+        end
+        @test_throws ErrorException macroexpand(@__MODULE__, ex)
+    end
+end
