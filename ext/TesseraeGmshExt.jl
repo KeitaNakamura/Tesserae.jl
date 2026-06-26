b5d932f0912ada03cc6189e07924244120dacd75
Keita Nakamura <keita.nakamura.1109@gmail.com>
2026-06-26 16:14:13 +0900

    Read Gmsh node coordinates

--- ext/TesseraeGmshExt.jl
+++ ext/TesseraeGmshExt.jl
@@ -4,6 +4,8 @@ using Tesserae
 using Gmsh
 using StaticArrays
 
+const GMSH_COORD_DIM = 3
+
 function from_gmsh_shape(name::String)
     name == "Line 2" && return Tesserae.Line2()
     name == "Line 3" && return Tesserae.Line3()
@@ -34,6 +36,27 @@ from_gmsh_connectivity(::Tesserae.Quad9) = SVector(1, 2, 3, 4, 5, 6, 7, 8, 9)
 from_gmsh_connectivity(::Tesserae.Hex8) = SVector(1, 2, 3, 4, 5, 6, 7, 8)
 from_gmsh_connectivity(::Tesserae.Hex20) = SVector(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 from_gmsh_connectivity(::Tesserae.Hex27) = SVector(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27)
+
+"""
+    read_gmsh_nodes()
+
+Read global nodes from the current Gmsh model and return `(nodes, nodeindices)`.
+`nodes` is a `Vector{Vec{dim,Float64}}`, and `nodeindices` maps each Gmsh
+node tag to the corresponding Tesserae node index.
+"""
+function read_gmsh_nodes()
+    node_tags, coords, _parametric_coords = Gmsh.gmsh.model.mesh.getNodes()
+    dim = Int(Gmsh.gmsh.model.getDimension())
+    nodes = Vector{Vec{dim, Float64}}(undef, length(node_tags))
+    nodeindices = Dict{eltype(node_tags), Int}()
+    sizehint!(nodeindices, length(node_tags))
+    @inbounds for i in eachindex(node_tags)
+        offset = GMSH_COORD_DIM * (i - 1)
+        nodes[i] = Vec{dim, Float64}(ntuple(j -> coords[offset + j], dim))
+        nodeindices[node_tags[i]] = i
+    end
+    nodes, nodeindices
+end
 
 function Tesserae.readmsh(filename::AbstractString; gmsh_argv=String[])
     initialized = Gmsh.initialize(gmsh_argv; finalize_atexit=false)
