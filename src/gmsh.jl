d687b00c4c15e9936568088d9c22d3d1c8f5918f
Keita Nakamura <keita.nakamura.1109@gmail.com>
2026-06-26 20:22:32 +0900

    Document readmsh

--- src/gmsh.jl
+++ src/gmsh.jl
@@ -1,1 +1,20 @@
+"""
+    readmsh(filename::AbstractString; gmsh_argv=String[])
+
+Read a Gmsh `.msh` file.
+
+This method is provided by the Gmsh extension. Load `Gmsh` before calling it:
+
+```julia
+using Tesserae
+using Gmsh
+
+meshes = readmsh("mesh.msh")
+```
+
+Returns a `Dict{String,UnstructuredMesh}` keyed by physical group name.
+Each physical group is read as one `UnstructuredMesh` and must contain exactly
+one supported element shape. Unnamed physical groups use keys of the form
+`"physical_group[dim,tag]"`.
+"""
 function readmsh end
